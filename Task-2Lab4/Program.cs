e4f1a08c7f6f03966bf30b8c041e6370ef8ca288
Anton Gusev <FrostTG@gmail.com>
2021-10-11 14:36:46 +0300

    Добавьте файлы проекта.

---
+++ Task-2Lab4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0, sum = 0, sum2 = 0;
+            do
+            {
+                N = Convert.ToInt32(Console.ReadLine());
+                if (N > 0)
+                {
+                    sum++;
+                }
+                else
+                {
+                    if (N < 0)
+                    {
+                        sum2++;
+                    }
+                }
+            } while (N != 0);
+            if (sum > sum2) //А можно ли этот оператор заменить тернарным?
+            {
+                Console.WriteLine("Положительных чисел больше");
+            }
+            else
+            {
+                if (sum < sum2)
+                {
+                    Console.WriteLine("Отрицательных чисел больше");
+                }
+                else
+                {
+
+                }
+
+            Console.ReadKey();
+            }
+        }
+    }
+}
